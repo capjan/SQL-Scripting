35523382f395f1418034fcdda2b5df2ceeeedff6
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-12-01 22:48:07 +0100

    Started Pragma state detection

---
+++ Core.SqlScripting/SQLite/Syntax/Statements/PragmaStatement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.SqlScripting.Common.Syntax;
+using Core.SqlScripting.Common.Syntax.Entity;
+
+namespace Core.SqlScripting.SQLite.Syntax.Statements
+{
+    public class PragmaStatement: ISqlStatement
+    {
+        public EntityObject Entity { get; set; }
+        public IPragmaValue Value  { get; set; }
+    }
+
+    public interface IPragmaValue
+    {
+
+    }
+
+    public class BooleanPragmaValue : IPragmaValue
+    {
+        public bool Value { get; set; }
+    }
+}
---
+++ Core.SqlScripting/SQLite/Writer/Statements/PragmaStatementFormatter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Core.SqlScripting.SQLite.Syntax.Statements;
+using Core.SqlScripting.SqlServer.Writer;
+using Core.Text.Formatter;
+
+namespace Core.SqlScripting.SQLite.Writer.Statements
+{
+    internal class PragmaStatementFormatter: ITextFormatter<PragmaStatement>
+    {
+        private readonly EntityObjectFormatter _entityObjectFormatter;
+
+        public PragmaStatementFormatter(EntityObjectFormatter entityObjectFormatter)
+        {
+            _entityObjectFormatter = entityObjectFormatter;
+        }
+
+        public void Write(PragmaStatement value, TextWriter writer)
+        {
+            writer.Write("PRAGMA ");
+            _entityObjectFormatter.Write(value.Entity, writer);
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class PragamaValueFormatter : ITextFormatter<IPragmaValue>
+    {
+        public void Write(IPragmaValue value, TextWriter writer)
+        {
+            if (value is BooleanPragmaValue boolValue)
+                writer.Write(boolValue.Value ? "1" : "0");
+            else 
+                throw new NotSupportedException();
+            
+        }
+    }
+}
